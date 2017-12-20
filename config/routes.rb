0be714afb86d753c4fd12af144a358b57f70d631
Bryan Hou <bryan.hou94@gmail.com>
2017-12-20 13:38:20 -0500

    add namespace to POST user_token, JWT auth should work now

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
+  scope '/api' do
+		post 'user_token' => 'user_token#create'
+  end
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   post 'signup' => 'registrations#create'
