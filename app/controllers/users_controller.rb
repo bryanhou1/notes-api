9d894133e0073d41f9dda29ec053c7b1976d696c
Bryan Hou <bryan.hou94@gmail.com>
2017-12-20 14:55:10 -0500

    add pry gem and build CRUD (no destroy yet) for users in user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+	# before_action :authenticate_user, except: :create
+
+	def show
+		@user = User.find(params[:id])
+		render json: @user
+	end
+
+	def index
+		@users = User.all
+		render json: @users
+	end
+
+	def destroy
+
+	end
+
+
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			render json: @user, status: 200
+		else
+			render json: { errors: @user.errors.full_messages }, status: 422
+		end
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			render json: @user, status: 201
+		else
+			render json: { errors: @user.errors.full_messages }, status: 422
+		end
+	end
+
+	private
+
+	def user_params
+		params.permit(:name, :username, :email, :password)
+	end
+
+end
