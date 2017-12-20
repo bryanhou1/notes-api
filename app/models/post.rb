4c55ac21e420fb9011f98b67d8fa0d53e5d6ea41
Bryan Hou <bryan.hou94@gmail.com>
2017-12-20 12:38:00 -0500

    add gem knock

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
 
 	belongs_to :user
-  has_and_belongs_to_many :pictures
+	has_and_belongs_to_many :pictures
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
   has_many :posts
-  has_many :pictures, through: :posts
+  has_many :pictures
 
 end
