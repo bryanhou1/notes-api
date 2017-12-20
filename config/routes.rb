c3934d33dd44aa86031398569a0d7b4a71f0b409
Bryan Hou <bryan.hou94@gmail.com>
2017-12-20 15:07:26 -0500

    sessions controller initiated and all routes moved into namespace /api

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   scope '/api' do
 		post 'user_token' => 'user_token#create'
-		resources 'users'
+		resources 'users', except: :create
+
+		post 'signup' => 'user#create'
+  	post 'signin' => 'sessions#create'
   end
 
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  post 'signup' => 'user#create'
-  post 'signin' => 'sessions#create'
+
 
   
 end
