262d34b85f7ee699344f97a24cd0c7dce1bc887d
Bryan Hou <bryan.hou94@gmail.com>
2018-01-10 04:50:08 -0500

    upgrade to pg and define ruby v for heroku deployment

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171220035849) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: :cascade do |t|
     t.string "title", default: "untitled"
     t.text "text"
